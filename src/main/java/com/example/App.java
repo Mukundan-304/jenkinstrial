99fef0d011332dba8cc21134c03c4bdfefe83b50
Mukundan-304 <mukundanponnusamy@gmail.com>
2026-01-07 22:30:57 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Maven build using Jenkins successful");
+        System.out.println("Commit");
     }
+
 }
